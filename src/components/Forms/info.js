65e5e4df28ddc060e9493d823e3c85626d7a2a52
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-07-26 01:03:24 -0300

    Add files via upload

---
+++ src/components/Forms/info.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useFormContext, useFieldArray } from "react-hook-form";
+
+const Info = () => {
+  const {
+    register,
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "info",
+  });
+
+  return (
+    <div>
+      {fields.map((item, index) => (
+        <div key={item.id}>
+          <input
+            placeholder="Adicione mais informações aqui"
+            {...register(`info[${index}].typeInfo`, { required: true })}
+          />
+          <p>{errors.info?.[index]?.typeInfo && "Preencha essa informação"}</p>
+          <button onClick={() => remove(index)} className="button-delete">
+            Deletar
+          </button>
+        </div>
+      ))}
+
+      <button
+        onClick={() => append({ typeInfo: "" })}
+        className="button-line-new"
+      >
+        Adicionar novo campo
+      </button>
+    </div>
+  );
+};
+
+export default Info;
